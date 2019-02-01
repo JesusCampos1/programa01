9a085b8ed69b0ca68759faaa4ff46bd4e91bb766
jsusc <jsusc@DESKTOP-SKGNO7S>
2019-01-31 19:32:50 -0600

    programa01

---
+++ progama01/src/progama01/programa01.java
@@ -0,0 +1,11 @@
+package progama01;
+
+public class programa01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programa uno con salida a pantalla");
+
+	}
+
+}
